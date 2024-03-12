2922009ec99f5f9f92babbaed83e37b163110d27
유정 박 <ckfkdl123@gmail.com>
2024-03-12 10:42:41 +0900

    숙제중

--- 0312자료구조/0312subject.c
+++ 0312자료구조/0312subject.c
@@ -1,11 +1,30 @@
 #include <stdio.h>
 
-int main(void) {
-	int n = 1;
-	while (n <= 100) {
-
-	}
+int main() {
 	
-	return 0;
+	int i, n;
+	int sum = 0;
+
+	for (n = 1; n <= 100; n++) {
+		if (prime(n)) {
+			sum += n;
+		}
+	}
+	pintf("1 ~ 100 까지 소수의 합 : %d", sum);
+
+}
+int prime(int n) {
+	int r, i;
+	int count = 0;
+	for (i = 2; i <= n; i++) {
+		r = n / i;
+		if (n == r * i) count++;
+
+		if (count > 1) return 0;
+	}
+
+	if (count == 1) return 1;
+	else
+		return 0;
 
 }
