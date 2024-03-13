833d48538eabb44454e020cc4f7c96b6c2afdec2
유정 박 <ckfkdl123@gmail.com>
2024-03-14 00:00:29 +0900

    100이하 소수 합

--- 0312자료구조/0312subject.c
+++ 0312자료구조/0312subject.c
@@ -1,28 +1,21 @@
 #include <stdio.h>
 
-int is_prime(int n) {
-    if (n <= 1) {
-        return 0; 
-    }
-    for (int i = 2; i * i <= n; i++) {
-        if (n % i == 0) {
-            return 0; 
-        }
-    }
-    return 1; 
-}
-
 int main() {
-    int limit = 100;
     int sum = 0;
+    int i, j;
 
-    for (int i = 2; i <= limit; i++) {
-        if (is_prime(i)) {
-            sum += i;
+    for (i = 2; i <= 100; i++) {
+        int primenumber = 1;
+
+        for (j = 2; j * j <= i; j++) {
+            int remainder = i % j;
+            int is_divisible = remainder == 0;
+            primenumber = primenumber && (!is_divisible);
         }
+
+        sum += (primenumber * i);
     }
 
-    printf("100 이하 소수 합: %d\n", sum);
-
+    printf("100 이하 소수들의 합: %d\n", sum);
     return 0;
 }
