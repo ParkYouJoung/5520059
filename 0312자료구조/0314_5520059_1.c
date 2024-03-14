62ac42c159023f7e429925cb7c7dd30968eae4fc
유정 박 <ckfkdl123@gmail.com>
2024-03-14 10:49:30 +0900

    과제

--- 0312자료구조/0314_5520059_1.c
+++ 0312자료구조/0314_5520059_1.c
@@ -5,12 +5,12 @@ int main(void)
 {
 	clock_t start, stop;
 	double duration;
-	start = clock();
+	start = clock();    // 측정 시작 
 
     int sum = 0;
     int i, j;
 
-    for (i = 2; i <= 100; i++) {
+    for (i = 2; i <= 100; i++) {                     // 100이하 소수들의 합 구하는 코드
         int primenumber = 1;
 
         for (j = 2; j * j <= i; j++) {
@@ -22,9 +22,8 @@ int main(void)
         sum += (primenumber * i);
     }
 
-    printf("100 이하 소수 합은 %d 입니다.\n", sum);
 
-    stop = clock();
+    stop = clock();                                      // 측정 종료
     duration = (double)(stop - start) / CLOCKS_PER_SEC;
     printf("수행시간은 %f초 입니다.\n", duration);
 
