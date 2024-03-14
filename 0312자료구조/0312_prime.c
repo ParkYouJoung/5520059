e16cf49fadcbd2365c6f253d2b467a6e2c71a422
유정 박 <ckfkdl123@gmail.com>
2024-03-14 11:01:56 +0900

    0314과제

--- 0312자료구조/0312_prime.c
+++ 0312자료구조/0312_prime.c
@@ -1,24 +1,24 @@
-//#include <stdio.h>
-//
-//int main() {
-//    int sum = 0;
-//    int i, j;
-//
-//    for (i = 2; i <= 100; i++) {
-//        int primenumber = 1;
-//
-//        for (j = 2; j * j <= i; j++) {
-//            int remainder = i % j;
-//            int div = remainder == 0;
-//            primenumber = primenumber && (!div);
-//        }
-//
-//        sum += (primenumber * i);
-//    }
-//
-//    printf("100 이하 소수 합은 %d 입니다.\n", sum);
-//
-//    return 0;
-//
-//    
-//}
+#include <stdio.h>
+
+int main() {
+    int sum = 0;
+    int i, j;
+
+    for (i = 2; i <= 100; i++) {
+        int primenumber = 1;
+
+        for (j = 2; j * j <= i; j++) {
+            int remainder = i % j;
+            int div = remainder == 0;
+            primenumber = primenumber && (!div);
+        }
+
+        sum += (primenumber * i);
+    }
+
+    printf("100 이하 소수 합은 %d 입니다.\n", sum);
+
+    return 0;
+
+    
+}
